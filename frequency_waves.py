8d3d40d472a3ed008d91ebf4957e639564c0cae9
hadar <hadarradah@gmail.com>
2020-02-03 17:37:19 +0200

    added DFT notebook

--- frequency_waves.py
+++ frequency_waves.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Util import v_to_db
 
+
 def nice_wave(N: int, Desired) -> np.array:
     # create the signal in time and dft domains
     sig_rnd = np.random.uniform(-1, 1, N)
@@ -11,7 +12,7 @@ def nice_wave(N: int, Desired) -> np.array:
 
     # build a "filter" - a vector that will make the random signal in the
     # desired shape
-    Desired = 10**(Desired/20)
+    Desired = 10 ** (Desired / 20)
     Filt = abs(Desired) / abs(Sig_rnd)
     Sig = Sig_rnd * Filt
     sig = np.fft.ifft(Sig)
@@ -22,15 +23,31 @@ def nice_wave(N: int, Desired) -> np.array:
     return sig
 
 
-# dictionary of different wave function, with their name as key.
+def square(omega, a, b, k=2 * np.pi):
+    sqr = np.array([1 if a < k_repeat(x, k) < b else 0 for x in omega])
+    return (sqr)
+
+
+def triangle(omega, center, r, k=2 * np.pi):
+    tri = np.array([max(0, (1 - (abs(k_repeat(x, k) - center) / r))) for x in omega])
+    return tri
+
+
+def circle(omega, o, r, k=2 * np.pi):
+    cir = np.array([np.sqrt(max(r ** 2 - abs(k_repeat(x, k) - o) ** 2, 0)) for x in omega])
+    return cir
+def dirac(omega, start, eps=0, k=2 * np.pi):
+    dirc = np.array([1 if start<=k_repeat(x,k)<start+eps else 0 for x in omega]) 
+    return dirc
+
+
 waves_dict = {
-    'square_HP': lambda x: max(0, np.sign(np.sin(x - np.pi / 2))),
-    'square_LP': lambda x: max(0, np.sign(np.sin(x + np.pi / 2))),
-    'triangle_LP': lambda x: max(0, (1 - (2 * abs(x) / np.pi))),
-    'triangle_HP': lambda x: max(0, (2 * abs(x) / np.pi) - 1),
-    'circle_LP': lambda x: np.sqrt(max((np.pi / 2) ** 2 - abs(x) ** 2, 0)),
-    'circle_HP': lambda x: np.sqrt(
-        max([(np.pi / 2) ** 2 - abs(x + np.pi) ** 2, (np.pi / 2) ** 2 - abs(x - np.pi) ** 2, 0]))
+    'square': square,
+
+    'triangle': triangle,
+
+    'circle': circle,
+
 }
 
 
@@ -71,32 +88,7 @@ def get_wave_with_omega(name: str) -> [np.array, np.array]:
     return omega, np.array([wave(two_pi_repeat(x)) for x in omega])
 
 
-def square(omega, a, b, k=2 * np.pi):
-    sqr = np.array([1 if a < k_repeat(x, k) < b else 0 for x in omega])
-    return (sqr)
-
-
-def reverse_square(omega, a, b, k=2 * np.pi):
-    sqr = np.array([0 if a < k_repeat(x, k) < b else 1 for x in omega])
-    return sqr
-
-
-def triangle(omega, center, r):
-    tri = np.array([max(0, (1 - (abs(two_pi_repeat(x) - center) / r))) for x in omega])
-    return tri
-
-
-def reverse_triangle(omega, r):
-    tri = np.array([max(0, (1 - (abs(x - min(omega)) / r)), 1 - (abs(x + min(omega)) / r)) for x in omega])
-    return tri
-
-
-def circle(omega, o, r):
-    cir = np.array([np.sqrt(max((r) ** 2 - abs(two_pi_repeat(x) - o) ** 2, 0)) for x in omega])
-    return cir
-
-
-def get_wave(name: str, omega: np.array) -> np.array:
+def get_wave(name: str, omega: np.array, a=np.pi / 2, b=np.pi / 2, k=2 * np.pi) -> np.array:
     """
 
    Parameters
@@ -106,21 +98,27 @@ def get_wave(name: str, omega: np.array) -> np.array:
        you can get all the names using get_keys() function
    omega :  np.array
        the frequencies for the wave
+   k: repeat freq
+   a: start
+   b: end
 
    Returns
    -------
    np.array
        the actual wave
+
+
    """
 
     wave = waves_dict.get(name)
-    Y = np.array([wave(two_pi_repeat(x)) for x in omega])
-    Y=nice_wave(len(Y),Y*60)
-    return ((Y))
+    Y = wave(omega, a, b, k)
+    Y = nice_wave(len(Y), Y * 60)
+
+    return Y
 
 
 import matplotlib.pyplot as plt
 
-#t = square(np.linspace(-np.pi, np.pi, 1000), np.pi / 2, 3 * np.pi / 4)
-#plt.plot(np.linspace(-np.pi, np.pi, 1000)/np.pi, t)
-#plt.show()
+t = square(np.linspace(-np.pi, np.pi, 1000), np.pi / 2, 3 * np.pi / 4)
+# plt.plot(np.linspace(-np.pi, np.pi, 1000)/np.pi, t)
+# plt.show()
